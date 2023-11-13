c2b559778f2df7b9118cefaaeb07c7d5b1452bd6
Bazilijus12 <114340327+Bazilijus12@users.noreply.github.com>
2023-11-14 00:43:41 +0200

    Changed language and navigation (#13)
    * Changed language
    
    Changed elements from english to lithuanian language.
    
    * Fixed registration form
    
    Changed and add some elements
    
    * Update Navigation.jsx
    
    Changed language

--- frontend/views/account/EditAccount.jsx
+++ frontend/views/account/EditAccount.jsx
@@ -31,35 +31,74 @@ function EditAccountPage() {
 
   return (
     <div className="container w-25 bg-white rounded">
-      <h1 className="text-center">Edit your account</h1>
+      <h1 className="text-center">Paskyros redagavimas</h1>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formName">
-          <Form.Label>Name</Form.Label>
+          <Form.Label>Vardas</Form.Label>
           <Form.Control
             type="text"
+            placeholder="Įveskitę vardą"
             value={user.firstname}
             onChange={(event) => setUser({ ...user, firstname: event.target.value })}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formSurname">
-          <Form.Label>Surname</Form.Label>
+          <Form.Label>Pavardė</Form.Label>
           <Form.Control
             type="text"
+            placeholder="Įveskitę pavardę"
             value={user.lastname}
             onChange={(event) => setUser({ ...user, lastname: event.target.value })}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formUsername">
-          <Form.Label>Username</Form.Label>
+        
+        <Form.Group className="mb-3" controlId="formGender">
+        <Form.Label>Lytis</Form.Label>
+        <Form.Control
+         as="select"
+         value={user.gender}
+         onChange={(event) => setUser({ ...user, gender: event.target.value })}
+  >
+        <option value="male">Vyras</option>
+        <option value="female">Moteris</option>
+        <option value="other">Kita</option>
+        </Form.Control>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formCity">
+          <Form.Label>Miestas</Form.Label>
           <Form.Control
             type="text"
-            value={user.username}
-            onChange={(event) => setUser({ ...user, username: event.target.value })}
+            placeholder="Įveskite miestą"
+            value={user.city}
+            onChange={(event) => setUser({ ...user, city: event.target.value })}
+          />
+        </Form.Group>
+
+
+        <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Elektroninis paštas</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Įveskite el. paštą"
+          value={user.email}
+          onChange={(event) => setUser({ ...user, email: event.target.value })}
+        />
+        </Form.Group>
+
+
+        <Form.Group className="mb-3" controlId="formPhoneNumber">
+          <Form.Label>Telefono numeris</Form.Label>
+          <Form.Control
+            type="tel"
+            placeholder="Įveskite telefono numerį"
+            value={user.phoneNumber}
+            onChange={(event) => setUser({ ...user, phoneNumber: event.target.value })}
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBirthdayDate">
-          <Form.Label>Birthday Date</Form.Label>
+          <Form.Label>Gimimo data</Form.Label>
           <Form.Control
             type="date"
             value={user.birthdayDate}
@@ -67,32 +106,17 @@ function EditAccountPage() {
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formLinkedInURL">
-          <Form.Label>LinkedIn URL</Form.Label>
+        <Form.Group className="mb-3" controlId="formLinkedIn">
+          <Form.Label>LinkedIn Profilio URL</Form.Label>
           <Form.Control
-            type="text"
-            value={user.linkedInUrl}
-            onChange={(event) => setUser({ ...user, linkedInUrl: event.target.value })}
+            type="url"
+            placeholder="Įveskitę LinkedIn profilio URL"
+            value={user.linkedin}
+            onChange={(event) => setUser({ ...user, linkedin: event.target.value })}
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formAbout">
-          <Form.Label>About</Form.Label>
-          <Form.Control
-            type="textarea"
-            value={user.about}
-            onChange={(event) => setUser({ ...user, about: event.target.value })}
-          />
-        </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formPhoneNumber">
-          <Form.Label>Phone number</Form.Label>
-          <Form.Control
-            type="text"
-            value={user.phoneNumber}
-            onChange={(event) => setUser({ ...user, phoneNumber: event.target.value })}
-          />
-        </Form.Group>
         <Button className="mb-3" variant="primary" type="submit">
           Save
         </Button>
