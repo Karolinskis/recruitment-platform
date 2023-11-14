c5e8a55f3e63520f33a87673e4c8185874eb90f4
Dominykas Valčiukas <domval1@ktu.lt>
2023-11-14 03:42:07 +0200

    Added 'Liked employer's list

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -36,6 +36,7 @@ import AdminUsersList from "../views/admin/UserList";
 // Errors
 import Error404 from "../views/Error404";
 import DeleteAccountPage from "../views/account/DeleteAccount";
+import FallowedWorkersListPage from "../views/employee/FallowedEmployersListPage";
 
 function AppRoutes() {
   return (
@@ -46,35 +47,49 @@ function AppRoutes() {
         <Route path="register" element={<CreateAccount />} />
 
         {/*<Route element={<RequireAuth/>}>*/}
-          <Route path="jobsAd/create" element={<JobAdCreate />} />
-          <Route path="jobsAd" element={<JobAd />} />
-          <Route path="dashboard" element={<DashBoard />} />
-          <Route path="account" element={<Account />} />
-          <Route path="account/passwordReset" element={<ChangePassword />} />
-          <Route path="account/deleteUser" element={<DeleteAccount />} />
+        <Route path="jobsAd/create" element={<JobAdCreate />} />
+        <Route path="jobsAd" element={<JobAd />} />
+        <Route path="dashboard" element={<DashBoard />} />
+        <Route path="account" element={<Account />} />
+        <Route path="account/passwordReset" element={<ChangePassword />} />
+        <Route path="account/deleteUser" element={<DeleteAccount />} />
 
-          <Route path="employer/hire-worker" element={<HireWorker />} />
-          <Route path="employer/rate-worker" element={<RateWorker/>} />
-          <Route path="employer/check-worker-availability" element={<CheckWorkerAvailability/>} />
-          <Route path="employer/delete-account" element={<DeleteEmployerAccount/>} />
-          <Route path="employer/send-email" element={<SendEmailToWorker/>} />
-          <Route path="employer/edit-offer" element={<EditJobOffer/>} />
+        <Route path="employer/hire-worker" element={<HireWorker />} />
+        <Route path="employer/rate-worker" element={<RateWorker />} />
+        <Route
+          path="employer/check-worker-availability"
+          element={<CheckWorkerAvailability />}
+        />
+        <Route
+          path="employer/delete-account"
+          element={<DeleteEmployerAccount />}
+        />
+        <Route path="employer/send-email" element={<SendEmailToWorker />} />
+        <Route path="employer/edit-offer" element={<EditJobOffer />} />
 
-          <Route path="account/edit" element={<EditAccount />} />
-          <Route path="jobsad/id" element={<JobOffer />} />
-          <Route path="jobOffer" element={<JobOffer/>} />
-          <Route path="createAccount" element={<CreateRecruiterAccount />} />
-          <Route path="job" element={<JobAd />} />
-          <Route path="jobsad/edit/:id" element={<JobAdEdit />} />
-          <Route path="jobsad-view/:id" element={<JobAdView />} />
-          <Route path="job/create" element={<JobOfferCreate />} />
-        
+        <Route path="account/edit" element={<EditAccount />} />
+        <Route path="jobsad/id" element={<JobOffer />} />
+        <Route path="jobOffer" element={<JobOffer />} />
+        <Route path="createAccount" element={<CreateRecruiterAccount />} />
+        <Route path="job" element={<JobAd />} />
+        <Route path="jobsad/edit/:id" element={<JobAdEdit />} />
+        <Route path="jobsad-view/:id" element={<JobAdView />} />
+        <Route path="job/create" element={<JobOfferCreate />} />
+
         {/* Admin */}
-          <Route path="admin/dashboard" element={<AdminDashBoard />} />
-          <Route path="admin/offendingContent/:type" element={<OffendingContent />} />
-          <Route path="admin/posts" element={<AdminPostsList />} />
-          <Route path="admin/users" element={<AdminUsersList />} />
+        <Route path="admin/dashboard" element={<AdminDashBoard />} />
+        <Route
+          path="admin/offendingContent/:type"
+          element={<OffendingContent />}
+        />
+        <Route path="admin/posts" element={<AdminPostsList />} />
+        <Route path="admin/users" element={<AdminUsersList />} />
 
+        {/* Domas */}
+        <Route
+          path="employee/FallowedEmployers"
+          element={<FallowedWorkersListPage />}
+        ></Route>
         <Route path="*" element={<Error404 />} />
       </Route>
     </Routes>
