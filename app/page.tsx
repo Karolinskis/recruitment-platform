4777de340022ecbb552f7f41b0727b849329fca1
Karolis <karolis.paulavicius2002@gmail.com>
2023-12-13 19:52:09 +0200

    Project rewrite (#21)
    * new-project
    
    * Edit gitignore
    
    * Add .env
    
    ---------
    
    Co-authored-by: Dominykas <94622539+M57D30@users.noreply.github.com>

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React from "react";
+import Jobs from "@/public/assets/jobs.jpeg";
+import Image from "next/image";
+import Button from "./components/Button/button";
+// import Component from "./components/Carouselle/carouselle";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="mx-10 mb-10">
+        <div className=" bg-white">
+          <div className="pt-20 3xl:pl-60 pl-44 pb-20 bg-purple-600 relative">
+            <div className=" flex 3xl:mx-auto 3xl:fit 3xl:w-[80%]">
+              <Image alt="darbas" src={Jobs} className="mr-14 3xl:w-[550px] " />
+              <div className="w-[600px]">
+                <p className="text-white font-google_font text-[50px] outline-8 outline p-5 ">
+                  "Kažkada buvau bedarbis. Bet sugalvojau pradėt dirbti. Dėka
+                  šios nuostabios platformos įkūriau gana sėkmingą obuolių
+                  kompaniją" <br />
+                  <span className="text-base">
+                    {" "}
+                    - STYVENAS DARBAS (DŽOBSAS)
+                  </span>
+                </p>
+                {/* <p className="text-slate-300 font-semibold mt-3">
+                - STYVENAS DARBAS (DŽOBSAS)
+              </p> */}
+              </div>
+            </div>
+            <Button />
+          </div>
+        </div>
+      </div>
+      {/* Jei veliau turesiu laiko */}
+      {/* <Component /> */}
+    </div>
+  );
+}
--- frontend/src/Hooks/useAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "../Context/AuthProvider";
-
-import React from 'react'
-
-const useAuth = () => {
-  return useContext(AuthContext);
-}
-export default useAuth;
