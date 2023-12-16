84df6cba94c26cff74f3f35e09de5653aa556e3a
Karolis <68164180+Karolinskis@users.noreply.github.com>
2023-12-16 23:45:39 +0200

    Add offending content pages and API

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,78 @@
+"use client"
+import React, { useEffect } from 'react';
+import Link from "next/link";
+
+const Dashboard = () => {
+    const [offendingUsers, setOffendingUsers] = React.useState([]);
+    const [offendingRecruiterPosts, setOffendingRecruiterPosts] = React.useState([]);
+    const [offendingJobSeekerPosts, setOffendingJobSeekerPosts] = React.useState([]);
+
+    useEffect(() => {
+        fetch("/api/getAllOffendingUsers")
+            .then(res => res.json())
+            .then(data => setOffendingUsers(data))
+            .catch(err => console.log(err));
+    }, []);
+
+    useEffect(() => {
+        fetch("/api/getAllOffendingRecruiterPosts")
+            .then(res => res.json())
+            .then(data => setOffendingRecruiterPosts(data))
+            .catch(err => console.log(err));
+    }, []);
+
+    useEffect(() => {
+        fetch("/api/getAllOffendingJobSeekerPosts")
+            .then(res => res.json())
+            .then(data => setOffendingJobSeekerPosts(data))
+            .catch(err => console.log(err));
+    }, []);
+
+    const offendingUserProfilesCount = offendingUsers.length;
+    const offendingRecruiterPostsCount = offendingRecruiterPosts.length;
+    const offendingJobSeekerPostsCount = offendingJobSeekerPosts.length;
+    const offendingContentCount = offendingUserProfilesCount + offendingRecruiterPostsCount + offendingJobSeekerPostsCount;
+
+    const getCardColor = (count: number) => {
+        if (count >= 10) return "bg-red-300";
+        if (count >= 5) return "bg-yellow-200";
+        return "bg-green-200";
+    };
+
+    return (
+      <div className="p-6">
+        <h1 className="text-3xl font-bold mb-4">Administratoriaus skydelis</h1>
+        <div className="grid grid-cols-1 gap-4 max-w-screen-md mx-auto">
+          <div className={`border border-gray-200 rounded p-2 flex flex-col items-center justify-center ${getCardColor(offendingContentCount)}`}>
+            <h2 className="text-gray-500">Iš viso rasta nekorektiškų duomenų</h2>
+            <p className="text-2xl font-bold">{offendingContentCount}</p>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className={`border border-gray-200 rounded p-3 flex flex-col items-center space-y-4 ${getCardColor(offendingUserProfilesCount)}`}>
+              <h2 className="text-gray-500 flex-grow">Nekorektiškos paskyros</h2>
+              <p className="text-2xl font-bold">{offendingUserProfilesCount}</p>
+              <Link href="/admin/offending-content/users">
+                <p className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">Peržiūrėti</p>
+              </Link>
+            </div>
+            <div className={`border border-gray-200 rounded p-3 flex flex-col items-center space-y-4 ${getCardColor(offendingRecruiterPostsCount)}`}>
+              <h2 className="text-gray-500 flex-grow">Nekorektiški darbdavių skelbimai</h2>
+              <p className="text-2xl font-bold mt-auto">{offendingRecruiterPostsCount}</p>
+              <Link href="/admin/offending-content/recruiter-posts" className="mt-auto">
+                <p className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">Peržiūrėti</p>
+              </Link>
+            </div>
+            <div className={`border border-gray-200 rounded p-3 flex flex-col items-center space-y-4 ${getCardColor(offendingJobSeekerPostsCount)}`}>
+              <h2 className="text-gray-500 flex-grow">Nekorektiški darbuotojų skelbimai</h2>
+              <p className="text-2xl font-bold mt-auto">{offendingJobSeekerPostsCount}</p>
+              <Link href="/admin/offending-content/job-seeker-posts" className="mt-auto">
+                <p className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">Peržiūrėti</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Dashboard;
---
+++ app/admin/offending-content/job-seeker-posts/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+import React, { useEffect } from 'react';
+import Link from "next/link";
+
+const OffendingJobSeekerPosts = () => {
+  const [offendingJobSeekerPosts, setOffendingJobSeekerPosts] = React.useState([]);
+
+  useEffect(() => {
+    fetch("/api/getAllOffendingJobSeekerPosts")
+      .then(res => res.json())
+      .then(data => setOffendingJobSeekerPosts(data))
+      .catch(err => console.log(err));
+  }, []);
+
+
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl font-bold mb-4">Nekorektiški darbuotojų skelbimai</h1>
+      <div className="overflow-x-auto">
+        <table className="table-auto border-collapse w-full">
+          <thead>
+            <tr className="rounded-lg text-sm font-medium text-gray-700 text-left" style={{fontSize: '0.9674rem'}}>
+              <th className="px-4 py-2" style={{backgroundColor: '#f8f8f8'}}>ID</th>
+              <th className="px-4 py-2" style={{backgroundColor: '#f8f8f8'}}>Pavadinimas</th>
+              <th className="px-4 py-2" style={{backgroundColor: '#f8f8f8'}}>Aprašymas</th>
+              <th className="px-4 py-2" style={{backgroundColor: '#f8f8f8'}}>Valandinis atlyginimas</th>
+              <th className="px-4 py-2" style={{backgroundColor: '#f8f8f8'}}>Validuota</th>
+              <th className="px-4 py-2" style={{backgroundColor: '#f8f8f8'}}>Veiksmai</th>
+            </tr>
+          </thead>
+          <tbody className="text-sm font-normal text-gray-700">
+            {offendingJobSeekerPosts.map((item, index) => (
+                <tr key={index} className="hover:bg-gray-100 border-b border-gray-200 py-10">
+                  <td className="px-4 py-4">{item.id_Skelbimo_anketa}</td>
+                  <td className="px-4 py-4">{item.pavadinimas}</td>
+                  <td className="px-4 py-4">{item.aprasymas}</td>
+                  <td className="px-4 py-4">{item.valandinis_atlyginimas}</td>
+                  <td className="px-4 py-4">{item.validuota ? "Taip" : "Ne"}</td>
+                  <td className="px-4 py-4">
+                    <Link href={`/admin/offending-content/job-seeker-posts/${item.id_Skelbimo_anketa}`}>
+                      <p className="btn bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-block">Validuoti</p>
+                    </Link>
+                    <Link href={`/admin/offending-content/job-seeker-posts/${item.id_Skelbimo_anketa}`}>
+                      <p className="btn bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded inline-block">Ištrinti</p>
+                    </Link>
+                  </td>
+                </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default OffendingJobSeekerPosts;
